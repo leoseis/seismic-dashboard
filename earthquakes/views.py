74b2a7672668f60105e4e0565ac6513d47476d51
leoseis <eboscrib@yahoo.com>
2026-04-12 03:03:38 +0100

    added a new view function earth quake list

--- earthquakes/views.py
+++ earthquakes/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Earthquake
+from .serializers import EarthquakeSerializer
+
+
+@api_view(["GET"])
+def earthquakes_list(request):
+
+    earthquakes = Earthquake.objects.all().order_by("-time")[:100]
+
+    serializer = EarthquakeSerializer(earthquakes, many=True)
+
+    return Response(serializer.data)
+
 # Create your views here.
