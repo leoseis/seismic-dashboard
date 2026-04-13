b5e59c2a35f87eb16ecfd808d2ec2d91bbf8bd65
leoseis <eboscrib@yahoo.com>
2026-04-13 11:49:33 +0100

    Create the Management Command Structure

---
+++ earthquakes/management/commands/fetch_earthquakes.py
@@ -0,0 +1,57 @@
+import requests
+from django.core.management.base import BaseCommand
+from earthquakes.models import Earthquake
+from datetime import datetime
+
+
+USGS_URL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson"
+
+
+class Command(BaseCommand):
+
+    help = "Fetch earthquake data from USGS"
+
+    def handle(self, *args, **kwargs):
+
+        self.stdout.write("Fetching earthquake data...")
+
+        response = requests.get(USGS_URL)
+
+        data = response.json()
+
+        count = 0
+
+        for eq in data["features"]:
+
+            props = eq["properties"]
+            coords = eq["geometry"]["coordinates"]
+
+            magnitude = props["mag"]
+            location = props["place"]
+            timestamp = props["time"]
+
+            longitude = coords[0]
+            latitude = coords[1]
+            depth = coords[2]
+
+            time = datetime.fromtimestamp(timestamp / 1000)
+
+            earthquake, created = Earthquake.objects.get_or_create(
+
+                magnitude=magnitude or 0,
+                location=location,
+                time=time,
+
+                defaults={
+                    "longitude": longitude,
+                    "latitude": latitude,
+                    "depth": depth,
+                }
+            )
+
+            if created:
+                count += 1
+
+        self.stdout.write(self.style.SUCCESS(
+            f"{count} earthquakes saved to database"
+        ))
