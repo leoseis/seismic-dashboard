1d66dcd125ee2fd732490829f82244e185d99750
leoseis <eboscrib@yahoo.com>
2026-04-22 15:26:23 +0100

    Fix missing latest_earthquake view

--- earthquakes/urls.py
+++ earthquakes/urls.py
@@ -2,9 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("earthquakes/", views.earthquakes_list),
-    path("earthquakes/latest/", views.latest_earthquakes),
-    path("earthquakes/filter/", views.filter_earthquakes),
-    path("location/", views.get_location),
-    path("live-earthquakes/", views.live_earthquakes),  
+    path('earthquakes/', views.earthquakes_list),
+    # path('earthquakes/latest/', views.latest_earthquake),
+    path('earthquakes/filter/', views.filter_earthquakes),
+    path('live-earthquakes/', views.live_earthquakes),
 ]
